f792765366f5ea0e3f39dfeaedfd30066122f4ef
vbrmnd <vaibhavrmande@gmail.com>
2016-01-31 14:39:58 +0530

    Restructuring and fixing controller appService data reference

--- app/appController.js
+++ app/appController.js
@@ -1,11 +1,9 @@
 angular.module('quote-tab').controller('appController', ['$scope', 'appService', function($scope, appService) {
+
 	$scope.appService = appService;
 
+	$scope.appService.fetch();
 	$scope.appService.getAll(function(data) {
 		$scope.quotes = data;
-		$scope.$apply();
-		console.log($scope.quotes);
 	});
-
-	console.log($scope.quotes);
 }]);
--- app/appService.js
+++ app/appService.js
@@ -3,7 +3,8 @@ angular.module('quote-tab', []).service('appService', [function() {
 	this.data = [];
 
 	this.fetch = function() {
-		_this.data.push(['this is a test quote']);
+		_this.data.push('this is a test quote');
+		_this.data.push('Another Quote');
 	}
 
 	this.getAll = function(callback) {
